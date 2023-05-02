b6fd2f7c4356b241ef6d18366395ea1845f680ff
thiviergejonathan <thiviergejonathan@gmail.com>
2023-05-02 13:13:05 -0400

    test

--- exercice_1/1.js
+++ exercice_1/1.js
@@ -2,6 +2,8 @@
 	Écrivez une fonction qui prend en entrée une température en degrés Celsius et la convertit en degrés Fahrenheit.
 
 	°F = (°C × 9/5) + 32
+	
+	THIS IS THE TEST!
 */
 
 const celciusToFarenheit = celcius => { }
